4020f8fc399452de52ea9bb0e23dd52e7f5f2fab
julio.medeiros <julio.medeiros@medsenior.com.br>
2021-11-28 03:11:26 -0300

    feat: subindo projeto ja finalizado.

---
+++ Readme.md
@@ -0,0 +1,31 @@
+## Bot para aceitar partida no League of Legends
+
+O intuito desse projeto é aprendizado e claro criar um bot para automatizar o aceite de partida utilizando python.
+
+# Recursos
+
+O projeto possui as seguintes caracteristicas: Automaticamente aceitar a partida e disparar a notificação para o usuario.
+
+# Instalação
+
+Para o projeto na sua máquina, voce irá precisar realizar as seguintes instalações:
+
+Python - [Python](https://www.python.org/)
+Pip - [Pip](https://pypi.org/project/pip/)
+
+Após instalar os dois, você também irá precisar instalar as seguintes libs:
+pip install pyautogui
+pip install opencv-python
+
+obs: pode acontecer também de precisar da seguinte instalação:
+pip install Pillow --upgrade
+
+# Disparo de email
+
+Para o disparo de email, você precisa habilitar para que apps menos seguros façam o acesso:
+
+Documentação Google - [Documentação](https://support.google.com/accounts/answer/6010255#zippy=%2Cse-a-op%C3%A7%C3%A3o-acesso-a-app-menos-seguro-estiver-desativada-para-sua-conta%2Cse-a-op%C3%A7%C3%A3o-acesso-a-app-menos-seguro-estiver-ativada-para-sua-conta)
+
+# Executavel
+
+Também gerei um exe para quem quiser apenas utilizar =D
---
+++ bot-para-aceitar-partida-lol.py
@@ -0,0 +1,92 @@
+import pyautogui
+from time import sleep
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def clicar_para_aceitar(posicaoX, posicaoY):
+    pyautogui.moveTo(posicaoX, posicaoY)
+    pyautogui.click()
+
+
+def enviar_email_de_notificacao(email_de_disparo, senha_do_email_de_disparo, email_destinatario):
+    nao_deseja_receber_email = len(email_destinatario) == 0
+    if(nao_deseja_receber_email):
+        return
+
+    # Informações de disparo do email:
+    EMAIL_QUE_IRA_DISPARAR = email_de_disparo
+    SENHA_DO_EMAIL_DE_DISPARO = senha_do_email_de_disparo
+    MENSAGEM = "Estou passando para avisar, que sua fila foi aceita."
+
+    # criando intencia do objeto de mensagem
+    msg = MIMEMultipart()
+
+    msg['From'] = EMAIL_QUE_IRA_DISPARAR
+    msg['To'] = email_destinatario
+    msg['Subject'] = "LOL - Partida Encontrada"
+
+    # adicionando mensagem ao corpo
+    msg.attach(MIMEText(MENSAGEM, 'plain'))
+
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.ehlo()  # Pode ser omitido
+            server.starttls(context=context)
+            server.ehlo()  # Pode ser omitido
+            server.login(EMAIL_QUE_IRA_DISPARAR, SENHA_DO_EMAIL_DE_DISPARO)
+            server.sendmail(msg['From'], msg['To'], msg.as_string())
+    except:
+        print('Não foi possível enviar email de notificaçao para "' +
+              email_destinatario + '"')
+
+
+def verificar_tela():
+    posicao_do_botao = pyautogui.locateOnScreen('fila.png', confidence=0.7)
+
+    encontrou_o_botao_na_tela = posicao_do_botao != None
+    if(encontrou_o_botao_na_tela):
+        clicar_para_aceitar(posicao_do_botao.left, posicao_do_botao.top)
+        return True
+
+    return False
+
+
+def cadastrar_email_que_ira_enviar_notificao(email_de_disparo, senha_do_email_de_disparo, email_destinatario):
+    nao_possui_informacao_de_envio = len(
+        email_de_disparo) == 0 and len(senha_do_email_de_disparo) == 0
+    if(nao_possui_informacao_de_envio):
+        return
+
+    enviar_email_de_notificacao(
+        email_de_disparo, senha_do_email_de_disparo, email_destinatario)
+
+
+def verificar_se_possui_dados_de_disparo_e_de_envio():
+    email_de_disparo = input(
+        'Seu email que irá disparar a notificação(opcional): ').strip()
+
+    informou_email_de_disparo = len(email_de_disparo) != 0
+    if(informou_email_de_disparo):
+        senha_do_email_de_disparo = input(
+            'Senha do seu email que irá disparar a notificação(opcional): ').strip()
+        email_destinatario = input(
+            'Seu email que irá receber a notificação(opcional): ').strip()
+        return email_de_disparo, senha_do_email_de_disparo, email_destinatario
+
+
+def main():
+    email_de_disparo, senha_do_email_de_disparo, email_destinatario = verificar_se_possui_dados_de_disparo_e_de_envio()
+
+    while True:
+        encontrou_o_botao_na_tela = verificar_tela()
+        if(encontrou_o_botao_na_tela):
+            cadastrar_email_que_ira_enviar_notificao(
+                email_de_disparo, senha_do_email_de_disparo, email_destinatario)
+            sleep(6)
+
+
+main()
